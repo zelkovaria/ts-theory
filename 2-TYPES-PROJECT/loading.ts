6d9eb5e08449b730623eaa34ed1e68fba81df0ba
zelkovaria <hyein396@gmail.com>
2024-04-17 00:08:18 +0900

    2-3 loading 만들기

--- 2-TYPES-PROJECT/loading.ts
+++ 2-TYPES-PROJECT/loading.ts
@@ -3,24 +3,36 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  const printLoginState = (state: ResourceLoadState) => {
+    if (state.state === "loading") {
+      console.log("👀 loading...");
+    }
+    if (state.state === "success") {
+      console.log(`😃 ${state.response.body}!!`);
+    }
+    if (state.state === "fail") {
+      console.log("😱 no network");
+    }
+  };
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
