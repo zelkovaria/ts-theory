9aa8665829775fc10d8165a5475ec06a6f55f646
zelkovaria <hyein396@gmail.com>
2024-04-18 18:21:01 +0900

    9-2 index type 예제 정리

---
+++ 9-advanced/9-2-index.ts
@@ -0,0 +1,22 @@
+{
+  const obj = {
+    name: "ellie",
+  };
+  obj.name; //ellie
+  obj["name"]; //ellie
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: "male" | "female";
+  };
+
+  type Name = Animal["name"];
+  //Name의 type은 string이 됨 -> 동물에 있는 name이라는 키의 타입을 Name에 선언한거임
+  const text: Name = "hello";
+
+  type Gender = Animal["gender"]; //'male' | 'female'
+
+  type Keys = keyof Animal;
+  //Animal에 있는 모든 키를 타입으로 할당 -> 'name' | 'age' | 'gender'
+}
